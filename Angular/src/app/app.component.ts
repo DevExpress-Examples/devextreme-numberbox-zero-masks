375897415c807871b557a983f5ab67c46eb732de
Tom <artem.kurchenko@devexpress.com>
2023-11-22 20:58:51 +0400

    Add Angular

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,20 +1,83 @@
 import { Component } from '@angular/core';
-import { ClickEvent } from 'devextreme/ui/button';
+
+import { DxNumberBoxTypes } from 'devextreme-angular/ui/number-box';
+import { DxDataGridTypes } from 'devextreme-angular/ui/data-grid';
+import dxNumberBox from 'devextreme/ui/number_box';
+
+import { PaymentInfo, Service } from './app.service';
+
+type CustomContentReadyEvent = DxNumberBoxTypes.ContentReadyEvent & {
+  component: ExtendedNumberBox;
+};
+type ExtendedNumberBox = dxNumberBox & {
+  isLoaded?: boolean;
+};
 
 @Component({
   selector: 'app-root',
+  providers: [Service],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+
 })
+
 export class AppComponent {
-  title = 'Angular';
+  nonZeroFormat = '##.######';
 
-  counter = 0;
+  zeroFormat = '0#.######';
 
-  buttonText = 'Click count: 0';
+  currencyFormat = '$ #,##0.##';
 
-  onClick(e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  zeroFormatEditorOptions;
+
+  nonZeroFormatEditorOptions;
+
+  dataSource: PaymentInfo[];
+
+  constructor(service: Service) {
+    this.zeroFormatEditorOptions = {
+      format: this.nonZeroFormat,
+    };
+    this.nonZeroFormatEditorOptions = {
+      format: this.currencyFormat,
+    };
+    this.dataSource = service.getPayments();
+  }
+
+  onContentReady = (e: CustomContentReadyEvent): void => {
+    if (!e.component.isLoaded) {
+      e.component.isLoaded = true;
+      if (e.component.option('value') === 0) e.component.option('format', this.zeroFormat);
+    }
+  };
+
+  onNonZeroKeyDown = (e: DxNumberBoxTypes.KeyDownEvent): void => {
+    const value = e.component.option('value');
+    if (e.event?.key === '0' && value === null) {
+      e.component.option('format', this.zeroFormat);
+    } else if (e.event?.key === 'Backspace' && value === 0) {
+      e.component.option('format', '');
+    } else if (value != null && (value > -1 && value < 1)) {
+      e.component.option('format', this.zeroFormat);
+    } else {
+      e.component.option('format', this.nonZeroFormat);
+    }
+  };
+
+  onZeroKeyDown = (e: DxNumberBoxTypes.KeyDownEvent): void => {
+    if (e.event?.key === 'Backspace' && e.component.option('value') === 0) {
+      e.component.option('format', '');
+    } else {
+      e.component.option('format', this.currencyFormat);
+    }
+  };
+
+  onEditorPreparing(e: DxDataGridTypes.EditorPreparingEvent): void {
+    if (e.parentType !== 'filterRow') return;
+    if (e.dataField === 'PaymentId') {
+      e.editorOptions.onContentReady = this.onContentReady;
+      e.editorOptions.onKeyDown = this.onNonZeroKeyDown;
+    }
+    if (e.dataField === 'Amount') e.editorOptions.onKeyDown = this.onZeroKeyDown;
   }
 }
