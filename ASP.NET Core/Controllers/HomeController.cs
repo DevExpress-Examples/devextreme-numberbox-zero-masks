176fa92d2db8908a8a0d6b25b05d47d55817fda8
flagmanAndrew <andrey.sergeyev@devexpress.com>
2025-10-29 15:32:43 +0400

    asp core

--- ASP.NET Core/Controllers/HomeController.cs
+++ ASP.NET Core/Controllers/HomeController.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_NET_Core.Controllers;
+
 public class HomeController: Controller {
     public IActionResult Index() {
         return View();
--- ASP.NET Core/Models/PaymentInfo.cs
+++ ASP.NET Core/Models/PaymentInfo.cs
@@ -1,11 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ASP_NET_Core.Models {
-
     public class PaymentInfo {
         public int PaymentId { get; set; }
         public string ContactName { get; set; }
